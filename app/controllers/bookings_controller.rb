e2c6196d538717c8dc5ba9ec6a458b190c39f04e
Artem Rakov <scoff1k@gmail.com>
2017-09-16 16:24:11 +0300

    created methods in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,13 @@
 class BookingsController < ApplicationController
-
   def new
-
+    @booking = Booking.new
   end
 
   def create
 
+  end
+
+  def booking_params
+    params.require(:booking).permit(:first_name, :last_name, :email, :phone, :model, :content)
   end
 end
