9b6a1b43617685319b229461b2cc6450d29d4088
Artem Rakov <scoff1k@gmail.com>
2017-09-18 03:18:46 +0300

    added sidenav for mobile

--- app/assets/javascripts/dropdown.js
+++ app/assets/javascripts/dropdown.js
@@ -2,3 +2,9 @@ $('.dropdown-button').dropdown({
       hover: true, // Activate on hover
     }
   );
+
+$(document).ready(function(){
+  $(".button-collapse").sideNav({
+    edge: 'right'
+  });
+})
