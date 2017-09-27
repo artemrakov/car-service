5ed365d94d4b6e89c93c3fe2bab237c7df11f877
Artem Rakov <scoff1k@gmail.com>
2017-09-27 20:56:55 +0100

    added sjadj

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,6 @@ class UserMailer < ApplicationMailer
   #
   def booking(booking_id)
     @booking = Booking.find(booking_id)
-    mail(from: "Команда AWM <amwvrn@google.com>", to: "scoff1k@gmail.com", subject: "Бронь от #{booking.first_name}")
+    mail(from: "Команда AWM <amwvrn@google.com>", to: "scoff1k@gmail.com", subject: "Бронь от #{@booking.first_name}")
   end
 end
