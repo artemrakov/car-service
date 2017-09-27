fa093dd178b6eb9429771b5f5c261e8902f86b98
Artem Rakov <scoff1k@gmail.com>
2017-09-27 20:59:33 +0100

    fkakds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,6 @@ class Booking < ApplicationRecord
   validates :first_name, :email, :phone, presence: true
 
   def send_booking_email
-    UserMailer.booking(self.id).deliver_later
+    UserMailer.booking(self.id).deliver_now
   end
 end
