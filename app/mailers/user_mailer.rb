da113263d8c6453940b294eb523d92684684c6f3
Artem Rakov <scoff1k@gmail.com>
2017-10-07 07:29:33 +0100

    test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -9,5 +9,6 @@ class UserMailer < ApplicationMailer
     @booking = Booking.find(booking_id)
     # mail(to: "oooawm2015@mail.ru", subject: "Бронь от #{@booking.first_name}")
     mail(to: "rakov.artem97@gmail.com", subject: "Бронь от #{@booking.first_name}")
+    mail(to: "artem.rakov2017@yandex.ru", subject: "Бронь от #{@booking.first_name}")
   end
 end
