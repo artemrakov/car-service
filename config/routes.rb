d524741636975daef95f612694fef0e0577353aa
Artem Rakov <scoff1k@gmail.com>
2017-10-09 17:43:53 +0100

    added routes thanks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'thanks', to: 'pages#thanks'
   get 'find_us', to: 'pages#find_us'
   resources :bookings, only: [:new, :create]
 end
