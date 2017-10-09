4bf503991c3e48f069189ee6f6293c72ec41f45f
Artem Rakov <scoff1k@gmail.com>
2017-10-09 21:12:13 +0100

    set up sms thing

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  after_create :send_booking_email, :send_booking_sms
-
+  # after_create :send_booking_email, :send_booking_sms
+  after_create :send_booking_sms
   validates :first_name, :email, :phone, presence: true
 
   def send_booking_email
@@ -8,5 +8,10 @@ class Booking < ApplicationRecord
   end
 
   def send_booking_sms
+    require 'net/http'
+    require 'uri'
+
+    uri = URI.parse("https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER']}&text=Поступило бронирование в AWM. Имя клиента: #{first_name}. Номер клиента: #{phone}.Подробная инфорамация на почте&from=biznes")
+    p response = Net::HTTP.get_response(uri)
   end
 end
