7e8348d7dc5b64759a9ea9c8872438f0710f676e
Artem Rakov <scoff1k@gmail.com>
2017-10-09 18:27:26 +0100

    removing twillio

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -8,19 +8,5 @@ class Booking < ApplicationRecord
   end
 
   def send_booking_sms
-    require 'twilio-ruby'
-
-    account_sid = ENV["TWILLIO_SID"]
-    auth_token = ENV["TWILLIO_TOKEN"]
-
-    begin
-        @client = Twilio::REST::Client.new account_sid, auth_token
-        message = @client.messages.create(
-            body: "Postypilo bronirovanie v AWM. Podrobnaya info otpravlena na po4ty",
-            to: "+79103412842",    # Replace with your phone number
-            from: "+33644641142")  # Replace with your Twilio number
-    rescue Twilio::REST::TwilioError => e
-        puts e.message
-    end
   end
 end
