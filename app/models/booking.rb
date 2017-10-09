76e374699b71b15fdaad4d8fb99ff4ec38d2c36e
Artem Rakov <scoff1k@gmail.com>
2017-10-09 21:55:41 +0100

    added russian sms

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,5 @@
 class Booking < ApplicationRecord
-  # after_create :send_booking_email, :send_booking_sms
-  after_create :send_booking_sms
+  after_create :send_booking_email, :send_booking_sms
   validates :first_name, :email, :phone, presence: true
 
   def send_booking_email
@@ -11,7 +10,8 @@ class Booking < ApplicationRecord
     require 'net/http'
     require 'uri'
 
-    uri = URI.parse("https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER']}&text=Поступило бронирование в AWM. Имя клиента: #{first_name}. Номер клиента: #{phone}.Подробная инфорамация на почте&from=biznes")
-    p response = Net::HTTP.get_response(uri)
+    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER']}&text=Поступило бронирование в AWM. Имя клиента #{first_name} . Номер #{phone}. Подробная информация отпралена на почту.&from=biznes"
+    uri = Addressable::URI.parse(url)
+    response = Net::HTTP.get_response(uri)
   end
 end
