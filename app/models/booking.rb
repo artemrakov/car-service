655e3340abc73e0dbed6e383b4609124e00d4912
Artem Rakov <scoff1k@gmail.com>
2018-04-12 14:18:04 +0300

    change from deliver_later to deliver_now

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,13 +3,13 @@ class Booking < ApplicationRecord
   validates :first_name, :email, :phone, presence: true
 
   def send_booking_email
-    UserMailer.booking(self.id).deliver_later
+    UserMailer.booking(self.id).deliver_now
   end
 
   def send_booking_sms
     require 'net/http'
     require 'uri'
-    
+
     url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER']}&text=Поступило бронирование в AWM. Имя клиента #{first_name} . Номер #{phone}. Подробная информация отправлена на почту.&from=biznes"
     uri = Addressable::URI.parse(url)
     response = Net::HTTP.get_response(uri)
