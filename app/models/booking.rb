4eb077f8f830d9d2f8dee816c5408c34fc35c0a0
Artem Rakov <scoff1k@gmail.com>
2019-03-11 15:23:25 +0300

    update booking fileds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,16 +1,17 @@
 class Booking < ApplicationRecord
   after_create :send_booking_email, :send_booking_sms
-  validates :first_name, :email, :phone, presence: true
+  validates :phone, presence: true
 
   def send_booking_email
-    UserMailer.booking(self.id).deliver_now
+    UserMailer.booking(id).deliver_now
   end
 
   def send_booking_sms
     require 'net/http'
     require 'uri'
 
-    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER']}&text=Поступило бронирование в AWM. Имя клиента #{first_name} . Номер #{phone}. Подробная информация отправлена на почту.&from=biznes"
+    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_1']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
+    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_2']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
     uri = Addressable::URI.parse(url)
     response = Net::HTTP.get_response(uri)
   end
