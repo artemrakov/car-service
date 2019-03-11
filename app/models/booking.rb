bf1c60e57e3d8ff8cc86121a2d3023b6909839e8
Artem Rakov <scoff1k@gmail.com>
2019-03-11 15:36:45 +0300

    remove email and fix phone sms

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,8 @@
 class Booking < ApplicationRecord
-  after_create :send_booking_email, :send_booking_sms
+  require 'net/http'
+  require 'uri'
+
+  # after_create :send_booking_email, :send_booking_sms
   validates :phone, presence: true
 
   def send_booking_email
@@ -7,12 +10,14 @@ class Booking < ApplicationRecord
   end
 
   def send_booking_sms
-    require 'net/http'
-    require 'uri'
+    url_1 = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_1']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
+    url_2 = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_2']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
+    send_sms(url_1)
+    send_sms(url_2)
+  end
 
-    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_1']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
-    url = "https://gate.smsaero.ru/send/?user=#{ENV['SMS_LOGIN']}&password=#{ENV['SMS_PASSWORD']}&to=#{ENV['SMS_NUMBER_2']}&text=Поступило бронирование в AWM. Номер #{phone}.&from=biznes"
+  def send_sms(url)
     uri = Addressable::URI.parse(url)
-    response = Net::HTTP.get_response(uri)
+    Net::HTTP.get_response(uri)
   end
 end
